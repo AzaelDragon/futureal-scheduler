6b1149778e8f2ae034d19bc5b42c46fc514bc188
David S. García (Azæl) <azael@dragonspark.tech>
2020-03-01 21:16:20 -0500

    Feat: Design core layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,80 +1,39 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
-</body>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
+        <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('apple-touch-icon.png') }}" />
+        <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}" />
+        <link rel="stylesheet" href="{{ asset('css/roboto.css') }}" />
+        <link rel="stylesheet" href="{{ asset('css/all.min.css') }}" />
+        <link rel="stylesheet" href="{{ asset('css/all-anim.css') }}" />
+        <link rel="stylesheet" href="{{ asset('css/material-dashboard.min.css') }}" />
+        <title> Futureal - @yield('title') </title>
+    </head>
+    <body class="@yield('body-class')">
+        @yield('body')
+        <script src="{{ asset('js/core/jquery.min.js')}}" type="text/javascript"></script>
+        <script src="{{ asset('js/core/popper.min.js')}}" type="text/javascript"></script>
+        <script src="{{ asset('js/core/bootstrap-material-design.min.js')}}" type="text/javascript"></script>
+        <script src="{{ asset('js/plugins/perfect-scrollbar.jquery.min.js')}}" type="text/javascript"></script>
+        <script src="{{ asset('js/plugins/moment.min.js')}}" type="text/javascript"></script>
+        <script src="{{ asset('js/plugins/sweetalert2.js')}}" type="text/javascript"></script>
+        <script src="{{ asset('js/plugins/jquery.validate.min.js')}}" type="text/javascript"></script>
+        <script src="{{ asset('js/plugins/jquery.bootstrap-wizard.js')}}" type="text/javascript"></script>
+        <script src="{{ asset('js/plugins/bootstrap-selectpicker.js')}}" type="text/javascript"></script>
+        <script src="{{ asset('js/plugins/bootstrap-datetimepicker.min.js')}}" type="text/javascript"></script>
+        <script src="{{ asset('js/plugins/jquery.dataTables.min.js')}}" type="text/javascript"></script>
+        <script src="{{ asset('js/plugins/bootstrap-tagsinput.js')}}" type="text/javascript"></script>
+        <script src="{{ asset('js/plugins/jasny-bootstrap.min.js')}}" type="text/javascript"></script>
+        <script src="{{ asset('js/plugins/fullcalendar.min.js')}}" type="text/javascript"></script>
+        <script src="{{ asset('js/plugins/jquery-jvectormap.js')}}" type="text/javascript"></script>
+        <script src="{{ asset('js/plugins/nouislider.min.js')}}" type="text/javascript"></script>
+        <script src="{{ asset('js/plugins/core.js')}}" type="text/javascript"></script>
+        <script src="{{ asset('js/plugins/arrive.min.js')}}" type="text/javascript"></script>
+        <script src="{{ asset('js/plugins/chartist.min.js')}}" type="text/javascript"></script>
+        <script src="{{ asset('js/plugins/bootstrap-notify.js')}}" type="text/javascript"></script>
+        <script src="{{ asset('js/material-dashboard.js?v=2.1.1') }}" type="text/javascript"></script>
+    </body>
 </html>
