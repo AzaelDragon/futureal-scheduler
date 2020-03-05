87ab8c3982c1c447a86bed67602569fca57025c5
David S. García (Azæl) <azael@dragonspark.tech>
2020-03-05 01:17:51 -0500

    Feat: Add custom welcome message to main page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,16 @@
 @extends('layouts.page')
-@section('title', 'Inicio')
+@section('title', '¡Bienvenido!')
 @section('body-class', 'off-canvas-sidebar')
 @section('header-class', 'page-header error-page header-filter')
 @section('header-image', asset('img/bg1.jpg'))
+@section('filter-color', 'black')
 @section('content')
     <div class="content-center">
         <div class="row">
             <div class="col-md-12">
                 <h1 class="title">¡Hola!</h1>
+                @auth <h2>¡Bienvenido de vuelta, {{ Auth::user() -> name }}!</h2>
+                @endauth
                 <h3> Haz click en el botón <b><i class="fas fa-cloud-meatball"></i> Aplicación</b> en la parte superior izquierda para comenzar. </h3>
             </div>
         </div>
