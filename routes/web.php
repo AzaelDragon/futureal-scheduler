d44786a9fedef980f3396a6998679a6b5ea90474
David S. García (Azæl) <azael@dragonspark.tech>
2020-03-05 01:20:46 -0500

    Chore: Add rooms routes

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index') -> name('home');
+
+Route::resource('rooms', 'RoomController') -> except(['create', 'show', 'edit']);
