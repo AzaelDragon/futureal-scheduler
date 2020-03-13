7861f1540e13accf67c75e7858c7bb7a5526e2c0
David S. García (Azæl) <azael@dragonspark.tech>
2020-03-13 18:56:32 -0500

    Feat: Implement scheduling controller

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -7,11 +7,24 @@ use App\Building;
 use App\Room;
 use App\Schedule;
 use App\Subject;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Validator;
 
 class ScheduleController extends Controller {
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct() {
+
+        $this -> middleware('auth');
+
+    }
 
     /**
      * Display a listing of the resource.
@@ -30,11 +43,84 @@ class ScheduleController extends Controller {
     }
 
     /**
+     * Get a data JSON for fullCalendar.
+     *
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function calendar(Request $request) {
+
+        $rawData = Schedule::where('user', Auth::user() -> id) -> get();
+        $data = array();
+
+        foreach ($rawData as $entry) {
+
+            $dates = $this -> parse_dates($entry -> date, $entry -> block);
+
+            $obj = [
+                'id' => $entry -> id,
+                'title' => Subject::find($entry -> subject) -> name,
+                'start' => $dates[0],
+                'end' => $dates[1],
+                'block' => $entry -> block,
+                'subject' => $entry -> subject,
+                'room' => $entry -> room,
+                'className' => 'event-rose'
+            ];
+
+            array_push($data, $obj);
+
+        }
+
+        return Response::json($data);
+    }
+
+    private function parse_block_time($id) {
+        switch ($id) {
+            case 1:
+                return ['07:00:00', '09:00:00'];
+            break;
+            case 2:
+                return ['9:00:00', '11:00:00'];
+            break;
+            case 3:
+                return ['11:00:00', '13:00:00'];
+            break;
+            case 4:
+                return ['14:00:00', '16:00:00'];
+            break;
+            case 5:
+                return ['16:00:00', '18:00:00'];
+            break;
+            default:
+                return ['00:00:00', '02:00:00'];
+            break;
+        }
+    }
+
+    private function parse_dates ($date, $id) {
+
+        $duration = $this -> parse_block_time($id);
+
+        return [
+             str_replace('+00:00', 'Z', Carbon::parse($date . ' ' . $duration[0]) -> toIso8601String()),
+             str_replace('+00:00', 'Z', Carbon::parse($date . ' ' . $duration[0]) -> toIso8601String())
+        ];
+
+    }
+
+    private function americanize_date($date) {
+
+        return str_replace('/', '-', $date);
+
+    }
+
+    /**
      * Find the amount of registries associated to the authenticated user.
      *
      * @return int
      */
-    public static function findAmount() {
+    public static function find_amount() {
 
         $schedules = Schedule::where('user', Auth::user() -> id) -> orderBy('name', 'desc') -> get();
         return count($schedules);
@@ -50,7 +136,7 @@ class ScheduleController extends Controller {
     public function store(Request $request) {
 
         $rules = array(
-            'date' => 'required|date',
+            'date' => 'required',
             'block' => 'required',
             'subject' => 'required',
             'room' => 'required'
@@ -63,7 +149,7 @@ class ScheduleController extends Controller {
         }
 
         $schedule = new Schedule();
-        $schedule -> date = $request -> input('date');
+        $schedule -> date = $this -> americanize_date($request -> input('date'));
         $schedule -> block = $request -> input('block');
         $schedule -> subject = $request -> input('subject');
         $schedule -> room = $request -> input('room');
@@ -97,7 +183,7 @@ class ScheduleController extends Controller {
         }
 
         $schedule = Schedule::find($id);
-        $schedule -> date = $request -> input('date');
+        $schedule -> date = $this -> americanize_date($request -> input('date'));
         $schedule -> block = $request -> input('block');
         $schedule -> subject = $request -> input('subject');
         $schedule -> room = $request -> input('room');
